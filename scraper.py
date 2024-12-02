af28c4def7c45445d721ed69b29229d85031d53c
John Xu <haoxuanxu1995@gmail.com>
2024-12-01 21:41:16 -0500

    modify

--- scraper.py
+++ scraper.py
@@ -1,64 +1,77 @@
-try:
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    driver = webdriver.Chrome()
-except:
-    print ("AF: No Chrome webdriver installed")
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-
 import time
-import json
-import pandas as pd
-from datetime import datetime
-from config import ALLOWED_ELEMENT_TYPES,ICON_COLOR_MAP
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+from config import ALLOWED_ELEMENT_TYPES, ICON_COLOR_MAP
 from utils import reformat_scraped_data
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver.get("https://www.forexfactory.com/calendar?month=this")
 
-month =  datetime.now().strftime("%B")
+def scrape(query: str, output_prefix: str):
+    try:
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
 
-table = driver.find_element(By.CLASS_NAME, "calendar__table")
+        driver = webdriver.Chrome()
+    except:
+        print("AF: No Chrome webdriver installed")
+        driver = webdriver.Chrome(ChromeDriverManager().install())
 
-data = []
-previous_row_count = 0
-# Scroll down to the end of the page
-while True:
-    # Record the current scroll position
-    before_scroll = driver.execute_script("return window.pageYOffset;")
-    
-    # Scroll down a fixed amount
-    driver.execute_script("window.scrollTo(0, window.pageYOffset + 500);")
-    
-    # Wait for a short moment to allow content to load
-    time.sleep(2)
-    
-    # Record the new scroll position
-    after_scroll = driver.execute_script("return window.pageYOffset;")
-    
-    # If the scroll position hasn't changed, we've reached the end of the page
-    if before_scroll == after_scroll:
-        break
+    driver.get(f"https://www.forexfactory.com/calendar?month={query}")
+    table = driver.find_element(By.CLASS_NAME, "calendar__table")
 
-# Now that we've scrolled to the end, collect the data
-for row in table.find_elements(By.TAG_NAME, "tr"):
-    row_data = []
-    for element in row.find_elements(By.TAG_NAME, "td"):
-        class_name = element.get_attribute('class')
-        if class_name in ALLOWED_ELEMENT_TYPES:
-            if element.text:
-                row_data.append(element.text)
-            elif "calendar__impact" in class_name:
-                impact_elements = element.find_elements(By.TAG_NAME, "span")
-                for impact in impact_elements:
-                    impact_class = impact.get_attribute("class")
-                    color = ICON_COLOR_MAP[impact_class]
-                if color:
-                    row_data.append(color)
-                else:
-                    row_data.append("impact")
+    data = []
+    # Scroll down to the end of the page
+    while True:
+        # Record the current scroll position
+        before_scroll = driver.execute_script("return window.pageYOffset;")
 
-    if len(row_data):
-        data.append(row_data)
+        # Scroll down a fixed amount
+        driver.execute_script("window.scrollTo(0, window.pageYOffset + 500);")
 
-reformat_scraped_data(data,month)
+        # Wait for a short moment to allow content to load
+        time.sleep(2)
+
+        # Record the new scroll position
+        after_scroll = driver.execute_script("return window.pageYOffset;")
+
+        # If the scroll position hasn't changed, we've reached the end of the page
+        if before_scroll == after_scroll:
+            break
+
+    # Now that we've scrolled to the end, collect the data
+    for row in table.find_elements(By.TAG_NAME, "tr"):
+        row_data = []
+        for element in row.find_elements(By.TAG_NAME, "td"):
+            class_name = element.get_attribute("class")
+            if class_name in ALLOWED_ELEMENT_TYPES:
+                if element.text:
+                    row_data.append(element.text)
+                elif "calendar__impact" in class_name:
+                    impact_elements = element.find_elements(By.TAG_NAME, "span")
+                    for impact in impact_elements:
+                        impact_class = impact.get_attribute("class")
+                        color = ICON_COLOR_MAP[impact_class]
+                    if color:
+                        row_data.append(color)
+                    else:
+                        row_data.append("impact")
+
+        if len(row_data):
+            data.append(row_data)
+
+    reformat_scraped_data(data, output_prefix)
+
+
+if __name__ == "__main__":
+    cur_month_date: date = date.today()
+    next_month_date: date = cur_month_date + relativedelta(months=1)
+
+    cur_month_query_str: str = (
+        f"{cur_month_date.strftime('%b').lower()}.{cur_month_date.year}"
+    )
+    next_month_query_str: str = (
+        f"{next_month_date.strftime('%b').lower()}.{next_month_date.year}"
+    )
+
+    scrape(cur_month_query_str, "current_month")
+    scrape(next_month_query_str, "next_month")
--- utils.py
+++ utils.py
@@ -3,16 +3,16 @@ import re
 import json
 import pandas as pd
 
+
 def read_json(path):
     """
     Read JSON data from a file.
     Args: path (str): The path to the JSON file.
     Returns: dict: The loaded JSON data.
     """
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         data = json.load(f)
     return data
-
 
 
 def contains_day_or_month(text):
@@ -27,20 +27,19 @@ def contains_day_or_month(text):
         and the matched text (day or month) if found.
     """
 
-     # Regular expressions for days of the week and months
-    days_of_week = r'\b(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b'
-    months = r'\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b'
-    pattern = f'({days_of_week}|{months})'
-    
+    # Regular expressions for days of the week and months
+    days_of_week = r"\b(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b"
+    months = r"\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b"
+    pattern = f"({days_of_week}|{months})"
+
     match = re.search(pattern, text, re.IGNORECASE)
-    
+
     if not match:
-        return False,None
-    
+        return False, None
+
     matched_text = match.group(0)
     if re.match(days_of_week, matched_text, re.IGNORECASE):
         return True, matched_text
-  
 
 
 def find_pattern_category(text):
@@ -56,16 +55,16 @@ def find_pattern_category(text):
     """
 
     # Regular expressions for different patterns
-    time_pattern = r'\d{1,2}:\d{2}(am|pm)'
-    day_pattern = r'Day\s+\d+'
-    date_range_pattern = r'\d{1,2}(st|nd|rd|th)\s*-\s*\d{1,2}(st|nd|rd|th)'
-    tentative_pattern = r'\bTentative\b'
-    pattern = f'({time_pattern}|{day_pattern}|{date_range_pattern}|{tentative_pattern})'
+    time_pattern = r"\d{1,2}:\d{2}(am|pm)"
+    day_pattern = r"Day\s+\d+"
+    date_range_pattern = r"\d{1,2}(st|nd|rd|th)\s*-\s*\d{1,2}(st|nd|rd|th)"
+    tentative_pattern = r"\bTentative\b"
+    pattern = f"({time_pattern}|{day_pattern}|{date_range_pattern}|{tentative_pattern})"
     match = re.search(pattern, text, re.IGNORECASE)
 
     if not match:
-        return False,None,None
-    
+        return False, None, None
+
     matched_text = match.group(0)
     if re.match(time_pattern, matched_text, re.IGNORECASE):
         category = "time"
@@ -78,8 +77,9 @@ def find_pattern_category(text):
     else:
         category = "Unknown"
     return True, category, matched_text
-    
-def reformat_scraped_data(data,month):
+
+
+def reformat_scraped_data(data, month):
     """
     Reformat scraped data and save it as a DataFrame and a CSV file.
 
@@ -90,32 +90,34 @@ def reformat_scraped_data(data,month):
     Returns:
         pd.DataFrame: The reformatted data as a DataFrame.
     """
-    current_date = ''
-    current_time = ''
+    current_date = ""
+    current_time = ""
     structured_rows = []
 
     for row in data:
-        if len(row)==1 or len(row)==5:
+        if len(row) == 1 or len(row) == 5:
             match, day = contains_day_or_month(row[0])
             if match:
-                current_date = row[0].replace(day,"").replace("\n","")
-        if len(row)==4:
+                current_date = row[0].replace(day, "").replace("\n", "")
+        if len(row) == 4:
             current_time = row[0]
 
-        if len(row)==5:
+        if len(row) == 5:
             current_time = row[1]
-        
-        if len(row)>1:
+
+        if len(row) > 1:
             event = row[-1]
             impact = row[-2]
             currency = row[-3]
-            structured_rows.append([current_date,current_time,currency,impact,event])
-                
+            structured_rows.append(
+                [current_date, current_time, currency, impact, event]
+            )
 
-    df = pd.DataFrame(structured_rows,columns=['date','time','currency','impact','event'])
-    os.makedirs("news",exist_ok=True)
-    df.to_csv(f"news/{month}_news.csv",index=False)
+    df = pd.DataFrame(
+        structured_rows, columns=["date", "time", "currency", "impact", "event"]
+    )
+    df = df[df["impact"] == "red"]
+    os.makedirs("news", exist_ok=True)
+    df.to_csv(f"news/{month}_news.csv", index=False)
 
     return df
-
-
